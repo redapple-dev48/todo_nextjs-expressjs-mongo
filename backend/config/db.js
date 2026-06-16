e0a0484423f451e99841b701568810f08e26efaf
Marlon <apple.red48@outlook.com>
2026-06-16 12:21:24 -0400

    Update Mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,13 +4,14 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    let mongo_uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ac-7fkz1tq-shard-00-00.oherg7t.mongodb.net:27017,ac-7fkz1tq-shard-00-01.oherg7t.mongodb.net:27017,ac-7fkz1tq-shard-00-02.oherg7t.mongodb.net:27017/?ssl=true&replicaSet=atlas-fo7ymf-shard-0&authSource=admin&appName=Cluster0/${process.env.MONGO_DB}`
+    await mongoose.connect(mongo_uri);
     console.log("MongoDB connected");
   }
-  catch(err) {
+  catch (err) {
     console.error(err);
     process.exit(1);
   }
-} 
+}
 
 module.exports = connectDB;
