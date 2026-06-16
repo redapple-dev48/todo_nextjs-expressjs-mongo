9027095bfcbd79368999f07f64f756ee1ba9b1ef
Marlon <apple.red48@outlook.com>
2026-06-16 14:16:33 -0400

    set exact database host

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    let mongo_uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ac-7fkz1tq-shard-00-00.oherg7t.mongodb.net:27017,ac-7fkz1tq-shard-00-01.oherg7t.mongodb.net:27017,ac-7fkz1tq-shard-00-02.oherg7t.mongodb.net:27017/?ssl=true&replicaSet=atlas-fo7ymf-shard-0&authSource=admin&appName=Cluster0/${process.env.MONGO_DB}`
-    await mongoose.connect(mongo_uri);
+    let mongo_uri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@ac-7fkz1tq-shard-00-00.oherg7t.mongodb.net:27017,ac-7fkz1tq-shard-00-01.oherg7t.mongodb.net:27017,ac-7fkz1tq-shard-00-02.oherg7t.mongodb.net:27017/?ssl=true&replicaSet=atlas-fo7ymf-shard-0&authSource=admin&appName=Cluster0`
+    console.log(mongo_uri)
+    await mongoose.connect(mongo_uri, {
+      dbName: process.env.MONGO_DB
+    });
     console.log("MongoDB connected");
   }
   catch (err) {
